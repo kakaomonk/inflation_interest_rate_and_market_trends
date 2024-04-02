2065f1862029d62d5e8fff31582a695396350e91
Hyungsoo Park <hyungsoo.park@mail.utoronto.ca>
2024-04-01 20:15:48 -0400

    Minor changes

---
+++ scripts/00-install_packages.R
@@ -0,0 +1,29 @@
+#### Preamble ####
+# Purpose: Installs all packages for the 
+# Author: Rohan Alexander [...UPDATE THIS...]
+# Date: 11 February 2023 [...UPDATE THIS...]
+# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# License: MIT
+# Pre-requisites: [...UPDATE THIS...]
+# Any other information needed? [...UPDATE THIS...]
+
+
+#### Setting-up the packages ####
+install.packages("tidyverse")
+install.packages("haven")
+install.packages("here")
+install.packages("knitr")
+install.packages("truncnorm")
+install.packages("flextable")
+
+
+#### Load downloaded packages ####
+library(tidyverse)
+library(haven)
+library(here)
+library(knitr)
+library(truncnorm)
+library(flextable)
+
+
+
