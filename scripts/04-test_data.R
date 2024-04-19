88d9c561b3f3f99923e5a3c0b7cfb31f8336cda2
Hyungsoo Park <hyungsoo.park@mail.utoronto.ca>
2024-04-18 23:53:15 -0400

    Final commit (paper, test script)

--- scripts/04-test_data.R
+++ scripts/04-test_data.R
@@ -4,12 +4,19 @@
 # Date: 10 April 2024
 # Contact: hyungsoo.park@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
 
 
 #### Workspace setup ####
 library(tidyverse)
-# [...UPDATE THIS...]
+library(arrow)
 
 #### Test data ####
+all_cleaned = read_parquet(here("data/analysis_data/all_cleaned.parquet"))
+
+
+is.character(all_cleaned$Quarter[1:38])
+is.numeric(all_cleaned$Inflation[1:38])
+is.numeric(all_cleaned$Interest[1:38])
+is.numeric(all_cleaned$NASDAQ_close[1:38])
+is.numeric(all_cleaned$Bitcoin_close[1:38])
+is.Date(all_cleaned$Date[1:38])
